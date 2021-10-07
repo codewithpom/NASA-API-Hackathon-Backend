0d5c5e3693a3e2f4ca23068deeb4013bccd26321
Padmashree Jha <codewithpom@gmail.com>
2021-10-07 12:51:47 +0530

    Create Account
    Create account done.

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const fs = require('fs');
-const PORT = process.env.PORT || 5050
-
+const PORT = process.env.PORT || 5050;
+const bodyParser = require('body-parser');
 app = express();
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.post('/create_account', function(req, res){
-    let data = JSON.parse(fs.readFileSync("data/accounts.json").toString());
-    
+
+app.post('/create_account', async (req, res) => {
+    const payload = req.body.payload;
+    if (payload === undefined) {
+        res.json({
+            "Error": "Wrong Body"
+        });
+        return;
+    }
+
+    let data = JSON.parse(fs.readFileSync("data/bookmarks.json").toString());
+
+    if (Object.keys(data).includes(payload["user_id"])) {
+        res.json({ "Error": "Account already exists" });
+        return;
+    } else {
+        data[payload["user_id"].toString()] = [];
+        fs.writeFileSync("data/bookmarks.json", JSON.stringify(data));
+        res.json({
+            "Sucess": "Account Made Sucessfully"
+        })
+    }
+
 })
 
-
-app.listen(PORT, ()=>{
-    console.log('listening on port ' +PORT);
+app.listen(PORT, () => {
+    console.log('listening on port ' + PORT);
 })
